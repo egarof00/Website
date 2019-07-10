04a0ddf39ea132495e1feef77aa6ef8ddbc2ed60
egarof00 <52299648+egarof00@users.noreply.github.com>
2019-07-10 15:23:09 -0700

    Add files via upload

---
+++ website.js
@@ -0,0 +1,24 @@
+// window.alert= function(title, message){
+//   var myElementToShow= document.getElementById("website.html");
+//   myElementToShow.innerHTML = title + "</br>" + message;
+// }
+//
+// function(WINDOWS VIRUS WARNING, Suspicious Activity Detected. Your browser may be redirected as an anonymous activity is found. Major security glitch. To fix please call System Securtiy Support....jk it's a prank bro)
+
+function myFunction(){
+  var x = document.getElementById("myTopnav");
+  if (x.className == "topnav"){
+    x.className += "responsive";
+  } else{
+    x.className ="topnav"
+  }
+}
+
+// var textArray =[
+//   "https://media0.giphy.com/media/SggILpMXO7Xt6/giphy.gif", "https://media1.tenor.com/images/efc9f6ce8923eadf351b676f29bf943c/tenor.gif?itemid=5449984",
+//   "https://i2.wp.com/teamjimmyjoe.com/wp-content/uploads/2016/11/trump-clinton-godzilla-funny-gifs.gif?fit=464%2C261&ssl=1",
+//   "https://media2.giphy.com/media/l41lXkx9x8OTM1rwY/source.gif", "http://38.media.tumblr.com/10b24025ed07aed368d9256b0b892841/tumblr_nd0iv0wRNd1terwlso1_400.gif"
+// ];
+// var randomNumber=Math.floor(Math.random()*textArray.length);
+//
+// audioElement.setAttribute("src", textArray[randomNumber]);
